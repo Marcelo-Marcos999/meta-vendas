661da7f86a51ae548ed9805a1febe27f87616275
Marcelo-Marcos999 <developergomes.g1@gmail.com>
2026-01-26 14:56:49 +0000

    chore: add cors dependency and enable cross-domain cookies

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,20 @@
 import express, { type Request, Response, NextFunction } from "express";
+import cors from "cors";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  cors({
+    origin: "https://meta-vendas.vercel.app",
+    credentials: true,
+  })
+);
+
 
 app.use((req, res, next) => {
   const start = Date.now();
